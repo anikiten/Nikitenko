97207ca5d0fd8dc4bcee9dde9d9a728c0b269d5d
anikiten <>
2010-05-13 13:58:36 +0000

    xxx

--- JPTexamples/test/JPTAnalysis_cfg.py
+++ JPTexamples/test/JPTAnalysis_cfg.py
@@ -27,12 +27,13 @@ process.load("RecoJets.Configuration.RecoJPTJets_cff")
 process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
+    input = cms.untracked.int32(2)
 )
 ### For 219, file from RelVal
 process.source = cms.Source("PoolSource",
 fileNames = cms.untracked.vstring(
-'/store/relval/CMSSW_3_6_0_pre6/RelValQCD_Pt_3000_3500/GEN-SIM-RECO/START36_V4-v1/0011/FE30B408-D044-DF11-92FC-0026189438C1.root')
+# '/store/relval/CMSSW_3_6_0_pre6/RelValQCD_Pt_3000_3500/GEN-SIM-RECO/START36_V4-v1/0011/FE30B408-D044-DF11-92FC-0026189438C1.root')
+'/store/relval/CMSSW_3_5_5/RelValQCD_FlatPt_15_3000/GEN-SIM-RECO/MC_3XY_V25-v1/0007/E0707AC5-0F38-DF11-B1FD-0026189437E8.root')
 )
 
 
@@ -45,17 +46,19 @@ process.myjetplustrack = cms.EDFilter("JetPlusTrackAnalysis",
 # PFjetsL2K3 = cms.InputTag("ak5PFJetsL2L3"),
 # CaloJetsL2L3 = cms.InputTag("ak5CaloJetsL2L3"),
     TriggerResults = cms.InputTag("TriggerResults","","HLT")	
+#    only for Spring10 data redigi'ed from the Summer'09 samples
+#    TriggerResults = cms.InputTag("TriggerResults","","REDIGI")	
 )
 
 
 process.dump = cms.EDFilter("EventContentAnalyzer")
 
 # for 360 to be used with 35X datasets. It create JPT not L2L3 corrected jet collecion: JetPlusTrackZSPCorJetAntiKt5
-# process.p1 = cms.Path(process.recoJPTJets*process.myjetplustrack)
+process.p1 = cms.Path(process.recoJPTJets*process.ak5JPTJetsL2L3*process.myjetplustrack*process.dump)
 
 # for 360: access to not L2L3 corrected JPT jet collection: JetPlusTrackZSPCorJetAntiKt5
-process.p1 = cms.Path(process.myjetplustrack)
+# process.p1 = cms.Path(process.myjetplustrack)
 
 # for 360: create colection of L2L3 corrected JPT jets: ak5JPTJetsL2L3  
 # one need set of tags will be provided be JES
-# process.p1 = cms.Path(process.ak5JPTJetsL2L3*process.myjetplustrack)
+# process.p1 = cms.Path(process.ak5JPTJetsL2L3*process.dump)
