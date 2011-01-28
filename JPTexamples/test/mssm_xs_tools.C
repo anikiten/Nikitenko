07ee0e39eb9cc7a20d1f39b89c15f5a56b763ce1
anikiten <>
2011-01-28 09:44:06 +0000

    xxx

--- JPTexamples/test/mssm_xs_tools.C
+++ JPTexamples/test/mssm_xs_tools.C
@@ -16,6 +16,156 @@ mssm_xs_tools::mssm_xs_tools(){
 
 mssm_xs_tools::~mssm_xs_tools() {}
 
+void setTDRStyle(Int_t xlog, Int_t ylog) {
+
+  TStyle *tdrStyle = new TStyle("tdrStyle","Style for P-TDR");
+
+// For the canvas:
+  tdrStyle->SetCanvasBorderMode(0);
+  tdrStyle->SetCanvasColor(kWhite);
+  tdrStyle->SetCanvasDefH(600); //Height of canvas
+  tdrStyle->SetCanvasDefW(600); //Width of canvas
+  tdrStyle->SetCanvasDefX(0);   //POsition on screen
+  tdrStyle->SetCanvasDefY(0);
+
+// For the Pad:
+  tdrStyle->SetPadBorderMode(0);
+  // tdrStyle->SetPadBorderSize(Width_t size = 1);
+  tdrStyle->SetPadColor(kWhite);
+  tdrStyle->SetPadGridX(false);
+  tdrStyle->SetPadGridY(false);
+  tdrStyle->SetGridColor(0);
+  tdrStyle->SetGridStyle(3);
+  tdrStyle->SetGridWidth(1);
+
+// For the frame:
+  tdrStyle->SetFrameBorderMode(0);
+  tdrStyle->SetFrameBorderSize(1);
+  tdrStyle->SetFrameFillColor(0);
+  tdrStyle->SetFrameFillStyle(0);
+  tdrStyle->SetFrameLineColor(1);
+  tdrStyle->SetFrameLineStyle(1);
+  tdrStyle->SetFrameLineWidth(1);
+
+// For the histo:
+  // tdrStyle->SetHistFillColor(1);
+  // tdrStyle->SetHistFillStyle(0);
+  tdrStyle->SetHistLineColor(1);
+  tdrStyle->SetHistLineStyle(0);
+  tdrStyle->SetHistLineWidth(2);
+  // tdrStyle->SetLegoInnerR(Float_t rad = 0.5);
+  // tdrStyle->SetNumberContours(Int_t number = 20);
+
+  tdrStyle->SetEndErrorSize(4);
+  //  tdrStyle->SetErrorMarker(20);
+  //  tdrStyle->SetErrorX(0.);
+  
+  tdrStyle->SetMarkerStyle(20);
+
+//For the fit/function:
+  tdrStyle->SetOptFit(1);
+  tdrStyle->SetFitFormat("5.4g");
+  tdrStyle->SetFuncColor(1);
+  tdrStyle->SetFuncStyle(1);
+  tdrStyle->SetFuncWidth(1);
+
+//For the date:
+  tdrStyle->SetOptDate(0);
+  // tdrStyle->SetDateX(Float_t x = 0.01);
+  // tdrStyle->SetDateY(Float_t y = 0.01);
+
+// For the statistics box:
+  tdrStyle->SetOptFile(0);
+  tdrStyle->SetOptStat(0); // To display the mean and RMS:   SetOptStat("mr");
+  tdrStyle->SetStatColor(kWhite);
+  tdrStyle->SetStatFont(42);
+  tdrStyle->SetStatFontSize(0.025);
+  tdrStyle->SetStatTextColor(1);
+  tdrStyle->SetStatFormat("6.4g");
+  tdrStyle->SetStatBorderSize(1);
+  tdrStyle->SetStatH(0.1);
+  tdrStyle->SetStatW(0.15);
+  // tdrStyle->SetStatStyle(Style_t style = 1001);
+  // tdrStyle->SetStatX(Float_t x = 0);
+  // tdrStyle->SetStatY(Float_t y = 0);
+
+// Margins:
+  tdrStyle->SetPadTopMargin(0.05);
+  tdrStyle->SetPadBottomMargin(0.13);
+  tdrStyle->SetPadLeftMargin(0.13);
+  tdrStyle->SetPadRightMargin(0.05);
+
+// For the Global title:
+
+  tdrStyle->SetOptTitle(0);
+  tdrStyle->SetTitleFont(42);
+  tdrStyle->SetTitleColor(1);
+  tdrStyle->SetTitleTextColor(1);
+  tdrStyle->SetTitleFillColor(10);
+  tdrStyle->SetTitleFontSize(0.05);
+  // tdrStyle->SetTitleH(0); // Set the height of the title box
+  // tdrStyle->SetTitleW(0); // Set the width of the title box
+  // tdrStyle->SetTitleX(0); // Set the position of the title box
+  // tdrStyle->SetTitleY(0.985); // Set the position of the title box
+  // tdrStyle->SetTitleStyle(Style_t style = 1001);
+  // tdrStyle->SetTitleBorderSize(2);
+
+// For the axis titles:
+
+  tdrStyle->SetTitleColor(1, "XYZ");
+  tdrStyle->SetTitleFont(42, "XYZ");
+  tdrStyle->SetTitleSize(0.06, "XYZ");
+  // tdrStyle->SetTitleXSize(Float_t size = 0.02); // Another way to set the size?
+  // tdrStyle->SetTitleYSize(Float_t size = 0.02);
+  tdrStyle->SetTitleXOffset(0.9);
+  tdrStyle->SetTitleYOffset(1.05);
+  // tdrStyle->SetTitleOffset(1.1, "Y"); // Another way to set the Offset
+
+// For the axis labels:
+
+  tdrStyle->SetLabelColor(1, "XYZ");
+  tdrStyle->SetLabelFont(42, "XYZ");
+  tdrStyle->SetLabelOffset(0.007, "XYZ");
+  tdrStyle->SetLabelSize(0.05, "XYZ");
+
+// For the axis:
+
+  tdrStyle->SetAxisColor(1, "XYZ");
+  tdrStyle->SetStripDecimals(kTRUE);
+  tdrStyle->SetTickLength(0.03, "XYZ");
+  tdrStyle->SetNdivisions(510, "XYZ");
+  tdrStyle->SetPadTickX(1);  // To get tick marks on the opposite side of the frame
+  tdrStyle->SetPadTickY(1);
+
+// Change for log plots:
+  tdrStyle->SetOptLogx(xlog);
+  tdrStyle->SetOptLogy(ylog);
+  tdrStyle->SetOptLogz(0);
+
+// Postscript options:
+
+//  tdrStyle->SetPaperSize(7.5,7.5);
+
+  tdrStyle->SetPaperSize(15.,15.);
+
+//  tdrStyle->SetPaperSize(20.,20.);
+
+  // tdrStyle->SetLineScalePS(Float_t scale = 3);
+  // tdrStyle->SetLineStyleString(Int_t i, const char* text);
+  // tdrStyle->SetHeaderPS(const char* header);
+  // tdrStyle->SetTitlePS(const char* pstitle);
+
+  // tdrStyle->SetBarOffset(Float_t baroff = 0.5);
+  // tdrStyle->SetBarWidth(Float_t barwidth = 0.5);
+  // tdrStyle->SetPaintTextFormat(const char* format = "g");
+  // tdrStyle->SetPalette(Int_t ncolors = 0, Int_t* colors = 0);
+  // tdrStyle->SetTimeOffset(Double_t toffset);
+  // tdrStyle->SetHistMinimumZero(kTRUE);
+
+  tdrStyle->cd();
+}
+
+
 void mssm_xs_tools::help(){
   std::cout<<"this is the MSSM neutral Higgs xsec tool"<<std::endl;
   std::cout<<"it takes prefabricated 2D ROOT histograms to obtain xsections"<<std::endl;
@@ -581,9 +731,13 @@ Double_t mssm_xs_tools::Give_XsecUnc_pdfalphas68down_ggFh(Double_t mA, Double_t 
 
 void mssm_xs_tools::myanalysis(){
   std::cout<<"My analysis starts"<<std::endl;
+
   const Int_t nbhmass = 11;
   Double_t mass[nbhmass]={90., 100., 120., 130., 140., 160., 180., 200., 250., 300., 350.};
+  Double_t tanb_exp[nbhmass];
+
   Double_t xsect_x_Br_limit[nbhmass]={185.99, 148.32, 36.17, 22.42, 15.74, 8.77, 5.78, 4.35, 2.46, 1.58, 1.23};
+
   for(Int_t i = 0; i < nbhmass ; i++) {
     for(Int_t j = 10; j < 60; j ++) {
       Double_t tanb = 1.*j;
@@ -609,10 +763,29 @@ void mssm_xs_tools::myanalysis(){
       if(xsec_x_Br_pb > xsect_x_Br_limit[i]) {
 	std::cout<<" mass = "<< mass[i] <<" tanb = "<< tanb <<"  xsect x Br = " << xsec_x_Br_pb 
 	         <<" limit = " << xsect_x_Br_limit[i] << std::endl;
+	tanb_exp[i] = tanb;
 	break;
       }
     }
   }
+
+  setTDRStyle(0,0);
+  // create graphs
+  TCanvas* c1 = new TCanvas("X","Y",1);
+  TGraph* gr_tanb_exp  = new TGraph(nbhmass,mass,tanb_exp);
+  TAxis* xaxis = gr_tanb_exp->GetXaxis();
+  TAxis* yaxis = gr_tanb_exp->GetYaxis();
+  gr_tanb_exp->GetXaxis()->SetTitle("M_{A}, GeV");
+  gr_tanb_exp->GetYaxis()->SetTitle("tan #beta");
+  gr_tanb_exp->GetXaxis()->CenterTitle();
+  gr_tanb_exp->GetYaxis()->CenterTitle();
+  xaxis->SetLimits(90.,350.);
+  gr_tanb_exp->SetMarkerStyle(21);
+  gr_tanb_exp->SetMaximum(50.);
+  gr_tanb_exp->SetMinimum(0.);
+  gr_tanb_exp->Draw("ALP");
+  c1->SaveAs("matanb.gif");
+
 }
 
 
