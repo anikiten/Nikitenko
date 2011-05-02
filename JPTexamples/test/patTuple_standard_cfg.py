3977bd2bcfb107654fd8536cea2298f3ddbc74c7
anikiten <>
2011-05-02 17:26:55 +0000

    xxx

--- JPTexamples/test/patTuple_standard_cfg.py
+++ JPTexamples/test/patTuple_standard_cfg.py
@@ -2,6 +2,9 @@
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
 
+# comment this line for CMSSW_4_1_4
+process.GlobalTag.globaltag = cms.string('MC_42_V9::All')
+
 ## ------------------------------------------------------
 #  NOTE: you can use a bunch of core tools of PAT to
 #  taylor your PAT configuration; for a few examples
@@ -19,39 +22,43 @@ from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
 process.out.outputCommands+= ["keep *_selectedPatJets*_*_*"]  ##  (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
 
-#produce JPT jets
-
-# reset proper settings
-# process.load("RecoJets.JetPlusTracks.JetPlusTrackCorrections_cff")
-# process.JetPlusTrackZSPCorJetAntiKt5.LeakageMap = "CondFormats/JetMETObjects/data/CMSSW_362_TrackLeakage.txt"
-# process.JetPlusTrackZSPCorJetAntiKt5.ResponseMap = "CondFormats/JetMETObjects/data/CMSSW_362_response.txt"
-# process.JetPlusTrackZSPCorJetAntiKt5.EfficiencyMap = "CondFormats/JetMETObjects/data/CMSSW_362_TrackNonEff.txt"
-# process.JetPlusTrackZSPCorJetAntiKt5.UseEfficiency = True
-# process.JetPlusTrackZSPCorJetAntiKt5.UseZSP = True
-
-from RecoJets.JetPlusTracks.JetPlusTrackCorrections_cff import *
-JetPlusTrackZSPCorJetAntiKt5.LeakageMap = "CondFormats/JetMETObjects/data/CMSSW_362_TrackLeakage.txt"
-JetPlusTrackZSPCorJetAntiKt5.ResponseMap = "CondFormats/JetMETObjects/data/CMSSW_362_response.txt"
-JetPlusTrackZSPCorJetAntiKt5.EfficiencyMap = "CondFormats/JetMETObjects/data/CMSSW_362_TrackNonEff.txt"
-JetPlusTrackZSPCorJetAntiKt5.UseEfficiency = True
-JetPlusTrackZSPCorJetAntiKt5.UseZSP = True
-JetPlusTrackZSPCorJetAntiKt5.tagName = cms.vstring('ZSP_CMSSW390_Akt_05_PU0')
+# produce JPT jets: uncomment these lines for 4_1_X
+# from RecoJets.JetPlusTracks.JetPlusTrackCorrections_cff import *
+# JetPlusTrackZSPCorJetAntiKt5.LeakageMap = "CondFormats/JetMETObjects/data/CMSSW_362_TrackLeakage.txt"
+# JetPlusTrackZSPCorJetAntiKt5.ResponseMap = "CondFormats/JetMETObjects/data/CMSSW_362_response.txt"
+# JetPlusTrackZSPCorJetAntiKt5.EfficiencyMap = "CondFormats/JetMETObjects/data/CMSSW_362_TrackNonEff.txt"
+# JetPlusTrackZSPCorJetAntiKt5.UseEfficiency = True
+# JetPlusTrackZSPCorJetAntiKt5.UseZSP = True
+# JetPlusTrackZSPCorJetAntiKt5.tagName = cms.vstring('ZSP_CMSSW390_Akt_05_PU0')
 
 # if one wants another JEC like Fall10 put JecFall10.db file into DB using JEC_cff.py with following line: 
 # process.load("RecoJets.JetPlusTracks.JEC_cff")
 # both Fall10.db and JEC_cff.py files are under JPTexamples/test should be moved to RecoJets/JetPlusTracks/python
 # example how it is working for RECO/AOD analysis is JetShapeJPTAnalysis_mc_s_z2.py
 
-process.load("RecoJets.JetAssociationProducers.ak5JTA_cff")
-process.load("RecoJets.Configuration.RecoJPTJets_cff")
+#JES: comment these lines in case of CMSSW_4_1_X: preliminary Spring11 corrections for CMSSW_4_2_0
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.jec = cms.ESSource("PoolDBESSource",
+      DBParameters = cms.PSet(
+        messageLevel = cms.untracked.int32(0)
+        ),
+      timetype = cms.string('runnumber'),
+      toGet = cms.VPSet(
+      cms.PSet(
+            record = cms.string('JetCorrectionsRecord'),
+            tag    = cms.string('JetCorrectorParametersCollection_Jec11_V1_AK5JPT'),
+            label  = cms.untracked.string('AK5JPT')
+            )
+      ),
+      ## here you add as many jet types as you need (AK5Calo, AK5JPT, AK7PF, AK7Calo, KT4PF, KT4Calo)
+      connect = cms.string('frontier://FrontierPrep/CMS_COND_PHYSICSTOOLS')
+)
+process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
+################################################################################################################
 
-# JES
-# process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
-# process.load('JetMETCorrections.Configuration.JetCorrectionServices_cff')
-# process.ak5JPTL1Offset.useCondDB = False
-# process.ak5JPTL2Relative = process.ak5CaloL2Relative.clone( era='Summer10',algorithm = 'AK5JPT')
-# process.ak5JPTL3Absolute    = process.ak5CaloL3Absolute.clone( era='Summer10',algorithm = 'AK5JPT')
-# process.ak5JPTResidual = process.ak5CaloResidual.clone( era='Summer10',algorithm = 'AK5JPT')
+# uncomment for CMSSW_4_1_4
+# process.load("RecoJets.JetAssociationProducers.ak5JTA_cff")
+# process.load("RecoJets.Configuration.RecoJPTJets_cff")
 
 process.dump = cms.EDAnalyzer("EventContentAnalyzer")
 
@@ -59,14 +66,16 @@ process.dump = cms.EDAnalyzer("EventContentAnalyzer")
 
 from PhysicsTools.PatAlgos.tools.jetTools import *
 
-process.patJetCorrFactors.levels = ['L1Offset', 'L2Relative', 'L3Absolute', 'L2L3Residual']
+process.patJetCorrFactors.levels = ['L1Offset', 'L2Relative', 'L3Absolute']
+
+# process.patJetCorrFactors.levels = ['L2Relative', 'L3Absolute']
 
 addJetCollection( process,
                            cms.InputTag('JetPlusTrackZSPCorJetAntiKt5'),
                            'AK5','JPT',
                            doJTA        = True,
                            doBTagging   = True,
-                           jetCorrLabel = ('AK5JPT', ['L1Offset', 'L2Relative', 'L3Absolute', 'L2L3Residual']),
+                           jetCorrLabel = ('AK5JPT', ['L1Offset', 'L2Relative', 'L3Absolute']),
                            doType1MET   = False,
                            doL1Cleaning = False,
                            doL1Counters = False,
@@ -74,20 +83,23 @@ addJetCollection( process,
                            doJetID      = True,
                            jetIdLabel   = "ak5")
 ##
-## let it run
+## uncomment for CMSSW_4_1_4
+# process.p = cms.Path(
+#   process.ak5JTA*process.jetPlusTrackZSPCorJetAntiKt5*process.patDefaultSequence
+#    )
+
 process.p = cms.Path(
-   process.ak5JTA*process.jetPlusTrackZSPCorJetAntiKt5*process.patDefaultSequence
-    )
+   process.patDefaultSequence
+   )
 
 ## ------------------------------------------------------
 #  In addition you usually want to change the following
 #  parameters:
 ## ------------------------------------------------------
-#
-#   process.GlobalTag.globaltag =  ...    ##  (according to https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions)
 #                                         ##
 process.source.fileNames = [          ##
-    '/store/relval/CMSSW_4_1_3/RelValTTbar/GEN-SIM-RECO/START311_V2-v1/0037/648B6AA5-C751-E011-8208-001A928116C6.root'
+#    '/store/relval/CMSSW_4_1_3/RelValTTbar/GEN-SIM-RECO/START311_V2-v1/0037/648B6AA5-C751-E011-8208-001A928116C6.root'
+    '/store/relval/CMSSW_4_2_0/RelValTTbar/GEN-SIM-RECO/MC_42_V9-v1/0054/3CBCB401-935E-E011-8345-0026189437F8.root'
    ]                                     ##  (e.g. 'file:AOD.root')
 #                                         ##
 #   process.maxEvents.input = cms.untracked.int32(2)            ##  (e.g. -1 to run on all events)
