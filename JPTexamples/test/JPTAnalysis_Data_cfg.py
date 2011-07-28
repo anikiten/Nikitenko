73a49ffb885926e23aaedc5cc37cb96610f40269
anikiten <>
2011-07-28 06:02:28 +0000

    xxx

--- JPTexamples/test/JPTAnalysis_Data_cfg.py
+++ JPTexamples/test/JPTAnalysis_Data_cfg.py
@@ -7,9 +7,10 @@ process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
-process.GlobalTag.globaltag = cms.string('START311_V2::All')
+# process.GlobalTag.globaltag = cms.string('START311_V2::All')
 # process.GlobalTag.globaltag = cms.string('MC_311_V1::All')
 # process.GlobalTag.globaltag = cms.string('GR_R_311_V2::All')
+process.GlobalTag.globaltag = cms.string('FT_R_42_V13A::All') 
 
 # process.load("Configuration.StandardSequences.Services_cff")
 # process.load("Configuration.StandardSequences.Reconstruction_cff")
@@ -25,6 +26,7 @@ process.GlobalTag.globaltag = cms.string('START311_V2::All')
 # Summer10 corrections. For 4_1_X use pre420 corrections. For 420X use 420X KJS
 #
 process.load("RecoJets.Configuration.RecoJPTJets_cff")
+process.load("RecoJets.JetAssociationProducers.ak5JTA_cff")
 process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
 process.load('JetMETCorrections.Configuration.JetCorrectionServices_cff')
 
@@ -37,37 +39,39 @@ process.load('JetMETCorrections.Configuration.JetCorrectionServices_cff')
 #
 # Preliminary Spring11 corrections for CMSSW_4_2_0 to be used with 4_1_X for JPT
 #
-process.load("CondCore.DBCommon.CondDBCommon_cfi")
-process.jec = cms.ESSource("PoolDBESSource",
-      DBParameters = cms.PSet(
-        messageLevel = cms.untracked.int32(0)
-        ),
-      timetype = cms.string('runnumber'),
-      toGet = cms.VPSet(
-      cms.PSet(
-            record = cms.string('JetCorrectionsRecord'),
-            tag    = cms.string('JetCorrectorParametersCollection_Jec11_V1_AK5JPT'),
-            label  = cms.untracked.string('AK5JPT')
-            )
-      ),
-      ## here you add as many jet types as you need (AK5Calo, AK5JPT, AK7PF, AK7Calo, KT4PF, KT4Calo)
-      connect = cms.string('frontier://FrontierPrep/CMS_COND_PHYSICSTOOLS')
-)
-process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
+#process.load("CondCore.DBCommon.CondDBCommon_cfi")
+#process.jec = cms.ESSource("PoolDBESSource",
+#      DBParameters = cms.PSet(
+#        messageLevel = cms.untracked.int32(0)
+#        ),
+#      timetype = cms.string('runnumber'),
+#      toGet = cms.VPSet(
+#      cms.PSet(
+#            record = cms.string('JetCorrectionsRecord'),
+#            tag    = cms.string('JetCorrectorParametersCollection_Jec11_V1_AK5JPT'),
+#            label  = cms.untracked.string('AK5JPT')
+#            )
+#      ),
+#      ## here you add as many jet types as you need (AK5Calo, AK5JPT, AK7PF, AK7Calo, KT4PF, KT4Calo)
+#      connect = cms.string('frontier://FrontierPrep/CMS_COND_PHYSICSTOOLS')
+#)
+#process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10000)
+    input = cms.untracked.int32(2)
 )
 ### For 219, file from RelVal
 process.source = cms.Source("PoolSource",
 fileNames = cms.untracked.vstring(
- '/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/161/312/F2A79C25-0A58-E011-BE69-003048F024C2.root')
+# '/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/161/312/F2A79C25-0A58-E011-BE69-003048F024C2.root')
+  'rfio:/castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuons_May10ReReco_run161312_1_1_SQ6.root')
 )
 
 
 process.myjetplustrack = cms.EDAnalyzer("JetPlusTrackAnalysis_Data",
     HistOutFile = cms.untracked.string('JPTAnalysis_Data.root'),
 #    HistOutFile = cms.untracked.string('JPTAnalysis_MC.root'),
+    muons    = cms.InputTag("muons"),	
     calojets = cms.InputTag("ak5CaloJets"),
     jetsID  = cms.InputTag("ak5JetID"),
     JPTjets = cms.InputTag("JetPlusTrackZSPCorJetAntiKt5"),
@@ -87,6 +91,6 @@ process.dump = cms.EDAnalyzer("EventContentAnalyzer")
  
 # process.p1 = cms.Path(process.ak5JPTJetsL2L3*process.myjetplustrack)
 
-process.p1 = cms.Path(process.ak5JPTJetsL1L2L3*process.myjetplustrack)
+process.p1 = cms.Path(process.ak5JTA*process.jetPlusTrackZSPCorJetAntiKt5*process.ak5JPTJetsL1L2L3*process.myjetplustrack)
 
 # process.p1 = cms.Path(process.dump)
