c9dc082fe2ec382f84507faa6384b501b052d7ff
anikiten <>
2011-07-29 15:51:03 +0000

    xxx

--- DPS/plugins/DPSAnalyser.cc
+++ DPS/plugins/DPSAnalyser.cc
@@ -12,7 +12,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/ParameterSet/interface/InputTag.h"
+#include "FWCore/Utilities/interface/InputTag.h"
 
 #include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
 #include "SimGeneral/HepPDTRecord/interface/ParticleDataTable.h"
