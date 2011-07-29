caffde1677aa5b1d0b3f6eeba3ca88ee7b82561e
anikiten <>
2011-07-29 12:43:59 +0000

    xxx

--- JPTexamples/plugins/JetPlusTrackAnalysis_Data.cc
+++ JPTexamples/plugins/JetPlusTrackAnalysis_Data.cc
@@ -32,13 +32,12 @@
 #include "CondFormats/L1TObjects/interface/L1GtTriggerMenu.h"
 #include "CondFormats/DataRecord/interface/L1GtTriggerMenuRcd.h"
 
-// MC info
-#include "CLHEP/Vector/LorentzVector.h"
+// MC info and vectors
+// #include "CLHEP/Vector/LorentzVector.h"
 #include "CLHEP/Units/GlobalPhysicalConstants.h"
 #include "SimGeneral/HepPDTRecord/interface/ParticleDataTable.h"
 #include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
 //#include "CLHEP/HepPDT/DefaultConfig.hh"
-//
 #include "DataFormats/Math/interface/LorentzVector.h"
 #include "DataFormats/Math/interface/Vector3D.h"
 #include "Math/GenVector/VectorUtil.h"
@@ -96,11 +95,6 @@
 #include "TTree.h"
 #include "TH1.h"
 #include "TH2.h"
-//
-#include "DataFormats/Math/interface/LorentzVector.h"
-#include "DataFormats/Math/interface/Vector3D.h"
-#include "Math/GenVector/VectorUtil.h"
-#include "Math/GenVector/PxPyPzE4D.h"
 
 using namespace std;
 using namespace reco;
@@ -136,8 +130,10 @@ private:
   edm::InputTag jetsIDSrc;
   // calo jets
   edm::InputTag calojetsSrc; 
-  // JPT jets
+  // JPT jets raw
   edm::InputTag JPTjetsSrc;
+  // JPT jets L1L2L3 corr
+  edm::InputTag JPTjetsL1L2L3Src;
   // MC jet corrections
   //  string JetCorrectionMC;
   // PF jet corrections
@@ -146,15 +142,20 @@ private:
   edm:: InputTag srcTriggerResults_;
 
   // variables to store in ntpl
-  int     jjpt;
   int     run, event, nvertex;
-  double  PVx, PVy, PVz;
+  double  DZmin, PVx, PVy, PVz;
+  // di muon pass
+  double  mass_mumu;
+  // muon eta/phi/pT
+  std::vector<double> *EtaMu;
+  std::vector<double> *PhiMu;
+  std::vector<double> *PtMu;
+  std::vector<double> *dzmuon;
   // eta/phi/pt of raw calo jet from JPT
-
   std::vector<double> *EtaRaw;
   std::vector<double> *PhiRaw;
   std::vector<double> *EtRaw;
-  // eta/phi/pt of JPT jets
+  // eta/phi/pt of JPT corrected jets
   std::vector<double> *EtaJPT;
   std::vector<double> *PhiJPT;
   std::vector<double> *EtJPT;
@@ -162,10 +163,12 @@ private:
   std::vector<int>    *Ntrk;
   // in-cone-in vertex track with max pT
   std::vector<double> *pTtrkMax;
+  // beta
+  std::vector<double> *beta;
   // number of Pxl and silicon inner and outer layers used for trk. measurement
-  std::vector<int>    *NPxlMaxPtTrk;  
-  std::vector<int>    *NSiIMaxPtTrk;  
-  std::vector<int>    *NSiOMaxPtTrk;  
+  //  std::vector<int>    *NPxlMaxPtTrk;  
+  //  std::vector<int>    *NSiIMaxPtTrk;  
+  //  std::vector<int>    *NSiOMaxPtTrk;  
   // output root file and tree
   TFile*      hOutputFile ;
   TTree*      t1;
@@ -186,6 +189,10 @@ JetPlusTrackAnalysis_Data::beginJob()
 {
   using namespace edm;
 
+  EtaMu        = new std::vector<double>();
+  PhiMu        = new std::vector<double>();
+  PtMu         = new std::vector<double>();
+  dzmuon       = new std::vector<double>();
   EtaRaw       = new std::vector<double>();
   PhiRaw       = new std::vector<double>();
   EtRaw        = new std::vector<double>();
@@ -194,9 +201,10 @@ JetPlusTrackAnalysis_Data::beginJob()
   EtJPT        = new std::vector<double>();
   Ntrk         = new std::vector<int>();
   pTtrkMax     = new std::vector<double>();
-  NPxlMaxPtTrk = new std::vector<int>();
-  NSiIMaxPtTrk = new std::vector<int>();
-  NSiOMaxPtTrk = new std::vector<int>();
+  beta         = new std::vector<double>();;
+  //  NPxlMaxPtTrk = new std::vector<int>();
+  //  NSiIMaxPtTrk = new std::vector<int>();
+  //  NSiOMaxPtTrk = new std::vector<int>();
 
   // creating a simple tree
 
@@ -204,15 +212,21 @@ JetPlusTrackAnalysis_Data::beginJob()
 
   t1 = new TTree("t1","analysis tree");
 
-  t1->Branch("jjpt",&jjpt,"jjpt/I");
   t1->Branch("run",&run,"run/I");
   t1->Branch("event",&event,"event/I");
 
   t1->Branch("nvertex",&nvertex,"nvertex/I");
+  t1->Branch("DZmin",&DZmin,"DZmin/D");
 
   t1->Branch("PVx",&PVx,"PVx/D");
   t1->Branch("PVy",&PVy,"PVy/D");
   t1->Branch("PVz",&PVz,"PVz/D");
+
+  t1->Branch("mass_mumu",&mass_mumu,"mass_mumu/D");
+  t1->Branch("EtaMu","vector<double>",&EtaMu);
+  t1->Branch("PhiMu","vector<double>",&PhiMu);
+  t1->Branch("PtMu" ,"vector<double>",&PtMu);
+  t1->Branch("dzmuon" ,"vector<double>",&dzmuon);
 
   t1->Branch("EtaRaw","vector<double>",&EtaRaw);
   t1->Branch("PhiRaw","vector<double>",&PhiRaw);
@@ -220,11 +234,12 @@ JetPlusTrackAnalysis_Data::beginJob()
   t1->Branch("EtaJPT","vector<double>",&EtaJPT);
   t1->Branch("PhiJPT","vector<double>",&PhiJPT);
   t1->Branch("EtJPT" ,"vector<double>",&EtJPT);
-  t1->Branch("pTtrkMax" ,"vector<double>",&pTtrkMax);
   t1->Branch("Ntrk","vector<int>",&Ntrk);
-  t1->Branch("NPxlMaxPtTrk","vector<int>",&NPxlMaxPtTrk);
-  t1->Branch("NSiIMaxPtTrk","vector<int>",&NSiIMaxPtTrk);
-  t1->Branch("NSiOMaxPtTrk","vector<int>",&NSiOMaxPtTrk);
+  t1->Branch("pTtrkMax" ,"vector<double>",&pTtrkMax);
+  t1->Branch("beta" ,"vector<double>",&beta);
+  //  t1->Branch("NPxlMaxPtTrk","vector<int>",&NPxlMaxPtTrk);
+  //  t1->Branch("NSiIMaxPtTrk","vector<int>",&NSiIMaxPtTrk);
+  //  t1->Branch("NSiOMaxPtTrk","vector<int>",&NSiOMaxPtTrk);
 
   return ;
 }
@@ -256,8 +271,10 @@ JetPlusTrackAnalysis_Data::JetPlusTrackAnalysis_Data(const edm::ParameterSet& iC
   muonsSrc         = iConfig.getParameter<edm::InputTag>("Muons");
   // raw calo jets
   jetsIDSrc        = iConfig.getParameter<edm::InputTag>("jetsID");
-  // JPT jets
+  // JPT jets raw
   JPTjetsSrc       = iConfig.getParameter<edm::InputTag>("JPTjets");
+  // JPT L1L2L3 corrected
+  JPTjetsL1L2L3Src = iConfig.getParameter<edm::InputTag>("JPTjetsL1L2L3");
   // calo jets
   calojetsSrc      = iConfig.getParameter<edm::InputTag>("calojets");
   // MC corrections
@@ -346,43 +363,43 @@ JetPlusTrackAnalysis_Data::analyze(const edm::Event& iEvent, const edm::EventSet
   //  std::map<double,int> pTjptIndex;
 
   std::map<double,const JPTJet*> pTjptIndex;
+  std::map<double,const Muon*> pTMuonIndex;
 
-  jjpt = 0;
+  math::XYZTLorentzVector  muon1(0.,0.,0.,0.);
+  math::XYZTLorentzVector  muon2(0.,0.,0.,0.);
+
   run = iEvent.id().run();
   event = iEvent.id().event();
+
   nvertex = 0;
+
+  DZmin =   0.;
   PVx = -1000.; 
   PVy = -1000.; 
   PVz = -1000.;
+
+  mass_mumu = 0.;
+  EtaMu->clear();
+  PhiMu->clear();
+  PtMu->clear();
+  dzmuon->clear();
+
   EtaRaw->clear();
   PhiRaw->clear();
   EtJPT->clear();
   EtaJPT->clear();
   PhiJPT->clear();
   EtJPT->clear();
-  pTtrkMax->clear(); 
   Ntrk->clear();
-  NPxlMaxPtTrk->clear();
-  NSiIMaxPtTrk->clear();
-  NSiOMaxPtTrk->clear();
-
-  // initialize vector containing four highest Et gen jets > 20 GeV
-  //   vector<CLHEP::HepLorentzVector> gjets;
-  //   gjets.clear();
+  pTtrkMax->clear(); 
+  beta->clear(); 
+  //  NPxlMaxPtTrk->clear();
+  //  NSiIMaxPtTrk->clear();
+  //  NSiOMaxPtTrk->clear();
   
   // muons
   edm::Handle<RecoMuons> reco_muons;
   iEvent.getByLabel(muonsSrc, reco_muons );
-  RecoMuons::const_iterator imuon = reco_muons->begin(); 
-  RecoMuons::const_iterator jmuon = reco_muons->end();
-  for ( ; imuon != jmuon; ++imuon ) {
-    if ( imuon->innerTrack().isNull() ||
-	 !muon::isGoodMuon(*imuon,muon::TMLastStationTight) ||
-	 imuon->innerTrack()->pt() < 3.0 ) { continue; }
-    cout <<" muon pT = " << imuon->innerTrack()->pt()
-	 <<" eta = " << imuon->innerTrack()->eta()
-	 <<" phi = " << imuon->innerTrack()->phi() << endl;
-  }
 
   // get jet ID map
   edm::Handle<ValueMap<reco::JetID> > jetsID;
@@ -393,9 +410,13 @@ JetPlusTrackAnalysis_Data::analyze(const edm::Event& iEvent, const edm::EventSet
   edm::Handle<PFJetCollection> pfjetsakt5;
   iEvent.getByLabel("ak5PFJets", pfjetsakt5);
    
-  // JPT jets
+  // JPT jets raw
   edm::Handle<reco::JPTJetCollection> jptjets;
   iEvent.getByLabel(JPTjetsSrc, jptjets);
+
+  // JPT jets L1L2L3
+  edm::Handle<reco::JPTJetCollection> jptjetsl1l2l3;
+  iEvent.getByLabel(JPTjetsL1L2L3Src, jptjetsl1l2l3);
   
   // Calo jets
   edm::Handle<CaloJetCollection> calojets;
@@ -405,51 +426,94 @@ JetPlusTrackAnalysis_Data::analyze(const edm::Event& iEvent, const edm::EventSet
   edm::Handle<reco::VertexCollection> recVtxs;
   iEvent.getByLabel("offlinePrimaryVertices",recVtxs);
 
+  // reco vertex part
   int nvtx = 0;
   unsigned ntrkV = 0;
-  
+  double cDVZmin = 1000.;
   for(unsigned int ind = 0; ind < recVtxs->size(); ind++) 
     {
       if ( !((*recVtxs)[ind].isFake()) && ((*recVtxs)[ind].ndof() > 4) )
 	{
-	  nvtx = nvtx + 1;
-	  if(nvtx == 1) {
-	    PVx  = (*recVtxs)[ind].x();
-	    PVy  = (*recVtxs)[ind].y();
-	    PVz  = (*recVtxs)[ind].z();
 
+	  nvtx = nvtx + 1;
+
+	  double cPVx  = (*recVtxs)[ind].x();
+	  double cPVy  = (*recVtxs)[ind].y();
+	  double cPVz  = (*recVtxs)[ind].z();
+
+	  if(nvtx == 1) {
+	    PVx  = cPVx;
+	    PVy  = cPVy;
+	    PVz  = cPVz;
 	    //      ndof(PV)>=5.0
 	    //	     ntrkV = (*recVtxs)[ind].tracksSize();
-
 	    //	    reco::Vertex::trackRef_iterator ittrk;
 	    //	    for(ittrk =(*recVtxs)[ind].tracks_begin();ittrk != (*recVtxs)[ind].tracks_end(); ++ittrk) 
 	    //	      if( (*recVtxs)[ind].trackWeight(*ittrk)>0.5 ) ntrkV++;
+	  } else {
+	    double DZ = fabs(cPVz - PVz);
+	    if(DZ < cDVZmin) 
+	      {
+		cDVZmin = DZ;
+	      } 
 	  }
 	}
     }
 
   nvertex = nvtx;
+  DZmin = cDVZmin;
 
+  // access muons and jets
   if( nvtx >= 1 ) {
 
-     // PF jet energy corrections
-    //    const JetCorrector* correctorPF = JetCorrector::getJetCorrector (JetCorrectionPF, iSetup);
-    
-    // MC jet energy corrections
-    //    const JetCorrector* correctorMC = JetCorrector::getJetCorrector (JetCorrectionMC, iSetup);
-     
-    if(jptjets->size() != 0) {
+    //    cout <<" nvertex = " << nvertex <<" DZmin = " << DZmin << endl;
 
-      for(JPTJetCollection::const_iterator jptjet = jptjets->begin(); jptjet != jptjets->end(); ++jptjet ) { 
+    // muons
+    RecoMuons::const_iterator imuon = reco_muons->begin(); 
+    RecoMuons::const_iterator jmuon = reco_muons->end();
+    for ( ; imuon != jmuon; ++imuon ) {
+      if ( imuon->innerTrack().isNull() ||
+	   !muon::isGoodMuon(*imuon,muon::TMLastStationTight) ||
+	   imuon->innerTrack()->pt() < 3.0 ) { continue; }
+      pTMuonIndex[imuon->innerTrack()->pt()] = &(*imuon);
+    }
+    
+    // jets
+    if(jptjetsl1l2l3->size() != 0) {
+      
+      for(JPTJetCollection::const_iterator jptjet = jptjetsl1l2l3->begin(); jptjet != jptjetsl1l2l3->end(); ++jptjet ) { 
 	if(jptjet->pt() > 20.) {
 	  pTjptIndex[jptjet->pt()] = &(*jptjet);
 	}
       }
     }
   }
+  // fill muon variables
+  map<double,const Muon*>::reverse_iterator rmfirst(pTMuonIndex.end());
+  map<double,const Muon*>::reverse_iterator rmlast(pTMuonIndex.begin());
+  int imu = 0;
+  while (rmfirst != rmlast) {
+    
+    const Muon* muon = (*rmfirst).second;
+    
+    imu++;
+    math::XYZTLorentzVector muonc(muon->innerTrack()->px(),muon->innerTrack()->py(), muon->innerTrack()->pz(), muon->innerTrack()->p()); 
+    EtaMu->push_back(muon->innerTrack()->eta());
+    PhiMu->push_back(muon->innerTrack()->phi());
+    PtMu->push_back(muon->innerTrack()->pt());
+    double dzvtx = fabs(muon->innerTrack()->dz((*recVtxs)[0].position()) );
+    dzmuon->push_back(dzvtx);
+    if (imu == 1) muon1 = muonc; 
+    if (imu == 2) muon2 = muonc; 
+    rmfirst++;
+  }
+    
+  if(imu >= 2) {
+    mass_mumu = sqrt(muon1.Dot(muon2));
+  }
 
+  // fill jet variables
   int jc = 0;
-  //      for(map<double,int>::reverse_iterator it = pTjptIndex.end(); it != pTjptIndex.begin(); ++it) {
   map<double,const JPTJet*>::reverse_iterator rfirst(pTjptIndex.end());
   map<double,const JPTJet*>::reverse_iterator rlast(pTjptIndex.begin());
   while (rfirst != rlast) {
@@ -514,12 +578,13 @@ JetPlusTrackAnalysis_Data::analyze(const edm::Event& iEvent, const edm::EventSet
     EtaJPT->push_back(jptjet->eta());
     PhiJPT->push_back(jptjet->phi());
     EtJPT->push_back(jptjet->pt());
-    pTtrkMax->push_back(pTMax);
     Ntrk->push_back(npions);
-    NPxlMaxPtTrk->push_back(NLayersPxl);
-    NSiIMaxPtTrk->push_back(NLayersSiI);
-    NSiOMaxPtTrk->push_back(NLayersSiO);
-    
+    pTtrkMax->push_back(pTMax);
+    beta->push_back(jptjet->getSpecific().Zch);
+    //    NPxlMaxPtTrk->push_back(NLayersPxl);
+    //    NSiIMaxPtTrk->push_back(NLayersSiI);
+    //    NSiOMaxPtTrk->push_back(NLayersSiO);
+    /*
     cout <<" jpt jet pT = " << jptjet->pt()
 	 <<" jpt eta = " << jptjet->eta() 
 	 <<" jpt phi = " << jptjet->phi() 
@@ -529,9 +594,10 @@ JetPlusTrackAnalysis_Data::analyze(const edm::Event& iEvent, const edm::EventSet
 	 <<" Ntrk1 = " << pionsInVertexInCalo.size()
 	 <<" Ntrk2 = " << pionsInVertexOutCalo.size()
 	 <<" Zch = " << jptjet->getSpecific().Zch << endl; 
+    */
   }
   // fill tree
-  if(pTjptIndex.size() != 0) t1->Fill();
+  if( (mass_mumu >= 40.) && (pTjptIndex.size() != 0) ) t1->Fill();
 }
 
 //define this as a plug-in
