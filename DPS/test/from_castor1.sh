d6376200d598d3001cd425bc25d33e7bb74c0649
anikiten <>
2011-08-02 12:31:08 +0000

    xxx

---
+++ DPS/test/from_castor1.sh
@@ -0,0 +1,27 @@
+#!/bin/tcsh
+
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_10_1_unH.root /tmp/anikiten/DiMuons_May10ReReco_1.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_11_1_SkD.root /tmp/anikiten/DiMuons_May10ReReco_2.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_12_1_U2P.root /tmp/anikiten/DiMuons_May10ReReco_3.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_13_1_h6E.root /tmp/anikiten/DiMuons_May10ReReco_4.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_14_1_NDP.root /tmp/anikiten/DiMuons_May10ReReco_5.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_15_1_lzu.root /tmp/anikiten/DiMuons_May10ReReco_6.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_16_1_fVR.root /tmp/anikiten/DiMuons_May10ReReco_7.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_17_1_Y4e.root /tmp/anikiten/DiMuons_May10ReReco_8.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_18_1_CU1.root /tmp/anikiten/DiMuons_May10ReReco_9.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_19_1_KEy.root /tmp/anikiten/DiMuons_May10ReReco_10.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_1_1_565.root /tmp/anikiten/DiMuons_May10ReReco_11.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_20_1_DdS.root /tmp/anikiten/DiMuons_May10ReReco_12.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_21_1_A6r.root /tmp/anikiten/DiMuons_May10ReReco_13.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_22_1_sF5.root /tmp/anikiten/DiMuons_May10ReReco_14.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_23_1_eu1.root /tmp/anikiten/DiMuons_May10ReReco_15.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_24_1_dXN.root /tmp/anikiten/DiMuons_May10ReReco_16.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_2_1_QXN.root /tmp/anikiten/DiMuons_May10ReReco_17.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_3_1_UrK.root /tmp/anikiten/DiMuons_May10ReReco_18.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_4_1_8b4.root /tmp/anikiten/DiMuons_May10ReReco_19.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_5_1_15D.root /tmp/anikiten/DiMuons_May10ReReco_20.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_6_1_xGC.root /tmp/anikiten/DiMuons_May10ReReco_21.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_7_1_Lsv.root /tmp/anikiten/DiMuons_May10ReReco_22.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_8_1_6SM.root /tmp/anikiten/DiMuons_May10ReReco_23.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuAnalysis_Data_9_1_g88.root /tmp/anikiten/DiMuons_May10ReReco_24.root
+rfcp /castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuons_May10ReReco_1_1_FXc.root /tmp/anikiten/DiMuons_May10ReReco_25.root
