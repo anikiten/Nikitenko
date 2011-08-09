040d9a35fb85f42eb77af75770f269438841c075
anikiten <>
2011-08-09 15:08:11 +0000

    xxx

--- JPTexamples/test/patTuple_standard_cfg.py
+++ JPTexamples/test/patTuple_standard_cfg.py
@@ -1,9 +1,25 @@
 ## import skeleton process
+
+# process.load('PhysicsTools.PatAlgos.patSequences_cff')
+
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
+from PhysicsTools.PatAlgos.tools.coreTools import *
+removeMCMatching(process,['All'])
+
+process.load('RecoJets.JetProducers.kt4PFJets_cfi')
+process.load('RecoJets.Configuration.RecoPFJets_cff')
+
+process.kt6PFJets = process.kt4PFJets.clone( rParam = 0.6, doRhoFastjet = True )
+process.kt6PFJets.Rho_EtaMax = cms.double(2.5)
+process.rhoProcess = cms.Sequence( process.kt6PFJets )
+process.kt6PFJets.doRhoFastjet = True
+process.ak5PFJets.doAreaFastjet = True
 
 # use proper tag. corresponding to CMSSW version !
-process.GlobalTag.globaltag = cms.string('MC_42_V13::All')
+# process.GlobalTag.globaltag = cms.string('MC_42_V13::All')
+
+process.GlobalTag.globaltag = cms.string('GR_R_42_V19::All')
 
 ## ------------------------------------------------------
 #  NOTE: you can use a bunch of core tools of PAT to
@@ -87,9 +103,10 @@ addJetCollection( process,
 #   process.ak5JTA*process.jetPlusTrackZSPCorJetAntiKt5*process.patDefaultSequence
 #    )
 
-process.p = cms.Path(
-   process.patDefaultSequence
-   )
+process.p = cms.Path(process.rhoProcess
+                    +process.kt6PFJets
+                    +process.ak5PFJets   
+                    +process.patDefaultSequence)
 
 ## ------------------------------------------------------
 #  In addition you usually want to change the following
@@ -98,7 +115,8 @@ process.p = cms.Path(
 #                                         ##
 process.source.fileNames = [          ##
 #    '/store/relval/CMSSW_4_1_3/RelValTTbar/GEN-SIM-RECO/START311_V2-v1/0037/648B6AA5-C751-E011-8208-001A928116C6.root'
-    '/store/relval/CMSSW_4_2_0/RelValTTbar/GEN-SIM-RECO/MC_42_V9-v1/0054/3CBCB401-935E-E011-8345-0026189437F8.root'
+#    '/store/relval/CMSSW_4_2_0/RelValTTbar/GEN-SIM-RECO/MC_42_V9-v1/0054/3CBCB401-935E-E011-8345-0026189437F8.root'
+     '/store/data/Run2011A/Jet/RECO/PromptReco-v4/000/165/121/4C12EC82-7481-E011-8499-0030487C8CB8.root'
    ]                                     ##  (e.g. 'file:AOD.root')
 #                                         ##
 #   process.maxEvents.input = cms.untracked.int32(2)            ##  (e.g. -1 to run on all events)
@@ -108,4 +126,4 @@ process.source.fileNames = [          ##
 #   process.out.fileName = ...            ##  (e.g. 'myTuple.root')
 #                                         ##
 #   process.options.wantSummary = True    ##  (to suppress the long output at the end of the job)    
-process.selectedPatElectrons.cut = 'pt > 10. & abs(eta) < 12.'
+# process.selectedPatElectrons.cut = 'pt > 10. & abs(eta) < 12.'
