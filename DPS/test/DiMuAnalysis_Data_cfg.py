b47f992ee322a66b7b45a2df007976149db5f976
anikiten <>
2011-12-20 09:54:40 +0000

    xxx

--- DPS/test/DiMuAnalysis_Data_cfg.py
+++ DPS/test/DiMuAnalysis_Data_cfg.py
@@ -7,55 +7,56 @@ process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
-# process.GlobalTag.globaltag = cms.string('START311_V2::All')
-# process.GlobalTag.globaltag = cms.string('MC_311_V1::All')
-# process.GlobalTag.globaltag = cms.string('GR_R_311_V2::All')
-process.GlobalTag.globaltag = cms.string('FT_R_42_V13A::All') 
-
-# process.load("Configuration.StandardSequences.Services_cff")
-# process.load("Configuration.StandardSequences.Reconstruction_cff")
-# process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-# process.GlobalTag.globaltag = cms.string('MC_3XY_V21::All')
-# process.GlobalTag.globaltag = cms.string('GR_R_38X_V15::All')
-# process.GlobalTag.globaltag = cms.string('START39_V8::All')
-# process.load("Configuration.StandardSequences.Simulation_cff")
-# process.load("Configuration.StandardSequences.MixingNoPileUp_cff")
-# process.load("Configuration.StandardSequences.VtxSmearedGauss_cff")
-
-#
-# Summer10 corrections. For 4_1_X use pre420 corrections. For 420X use 420X KJS
-#
 process.load("RecoJets.Configuration.RecoJPTJets_cff")
 process.load("RecoJets.JetAssociationProducers.ak5JTA_cff")
 process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
 process.load('JetMETCorrections.Configuration.JetCorrectionServices_cff')
 
-#process.ak5JPTL1Offset.useCondDB = False
-#process.ak5JPTL2Relative = process.ak5CaloL2Relative.clone( era='Summer10',algorithm = 'AK5JPT' )
-#process.ak5JPTL3Absolute    = process.ak5CaloL3Absolute.clone( era='Summer10',algorithm = 'AK5JPT' )
-#process.ak5JPTResidual = process.ak5CaloResidual.clone( era='Summer10',algorithm = 'AK5JPT' )
-#
-# pre 420 corrections
-#
-# Preliminary Spring11 corrections for CMSSW_4_2_0 to be used with 4_1_X for JPT
-#
-#process.load("CondCore.DBCommon.CondDBCommon_cfi")
-#process.jec = cms.ESSource("PoolDBESSource",
-#      DBParameters = cms.PSet(
-#        messageLevel = cms.untracked.int32(0)
-#        ),
-#      timetype = cms.string('runnumber'),
-#      toGet = cms.VPSet(
-#      cms.PSet(
-#            record = cms.string('JetCorrectionsRecord'),
-#            tag    = cms.string('JetCorrectorParametersCollection_Jec11_V1_AK5JPT'),
-#            label  = cms.untracked.string('AK5JPT')
-#            )
-#      ),
-#      ## here you add as many jet types as you need (AK5Calo, AK5JPT, AK7PF, AK7Calo, KT4PF, KT4Calo)
-#      connect = cms.string('frontier://FrontierPrep/CMS_COND_PHYSICSTOOLS')
-#)
-#process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
+# process.GlobalTag.globaltag = cms.string('START311_V2::All')
+# process.GlobalTag.globaltag = cms.string('MC_311_V1::All')
+# process.GlobalTag.globaltag = cms.string('GR_R_311_V2::All')
+
+process.GlobalTag.globaltag = cms.string('FT_R_42_V13A::All') 
+
+# process.GlobalTag.globaltag = cms.string('GR_R_44_V4::All')
+
+####################################################################################
+from CondCore.DBCommon.CondDBSetup_cfi import *
+
+process.jec = cms.ESSource("PoolDBESSource",CondDBSetup,
+                           connect =
+cms.string("frontier://FrontierPrep/CMS_COND_PHYSICSTOOLS"),
+                   toGet =  cms.VPSet(
+                       cms.PSet(record = cms.string("JetCorrectionsRecord"),
+                                tag =
+cms.string("JetCorrectorParametersCollection_Jec11_V7_AK5Calo"),
+                                label=cms.untracked.string("AK5Calo")),
+                       cms.PSet(record = cms.string("JetCorrectionsRecord"),
+                                tag =
+cms.string("JetCorrectorParametersCollection_Jec11_V7_AK5PF"),
+                                label=cms.untracked.string("AK5PF")),
+                       cms.PSet(record = cms.string("JetCorrectionsRecord"),
+                                tag =
+cms.string("JetCorrectorParametersCollection_Jec11_V7_AK5PFchs"),
+                                label=cms.untracked.string("AK5PFchs")),
+                       cms.PSet(record = cms.string("JetCorrectionsRecord"),
+                                tag =
+cms.string("JetCorrectorParametersCollection_Jec11_V7_AK5JPT"),
+                                label=cms.untracked.string("AK5JPT")),
+                       cms.PSet(record = cms.string("JetCorrectionsRecord"),
+                                tag =
+cms.string("JetCorrectorParametersCollection_Jec11_V3_KT4Calo"),
+                                label=cms.untracked.string("KT4Calo")),
+                       cms.PSet(record = cms.string("JetCorrectionsRecord"),
+                                tag =
+cms.string("JetCorrectorParametersCollection_Jec11_V3_AK7Calo"),
+                                label=cms.untracked.string("AK7Calo")),
+                       )
+
+                   )
+process.es_prefer_jec = cms.ESPrefer("PoolDBESSource","jec")
+process.ak5JPTL1Offset.algorithm = 'AK5JPT'
+########################################################################################
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
@@ -63,8 +64,8 @@ process.maxEvents = cms.untracked.PSet(
 ### For 219, file from RelVal
 process.source = cms.Source("PoolSource",
 fileNames = cms.untracked.vstring(
-# '/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/161/312/F2A79C25-0A58-E011-BE69-003048F024C2.root')
-  'rfio:/castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuons_May10ReReco_run161312_1_1_SQ6.root')
+ '/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/161/312/F2A79C25-0A58-E011-BE69-003048F024C2.root')
+#  'rfio:/castor/cern.ch/user/a/anikiten/DiMuonData2011/DiMuons_May10ReReco_1_1_ggY.root')
 )
 
 
