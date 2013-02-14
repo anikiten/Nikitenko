da7be10d21181dd01c2b08da76be8fb18030104d
anikiten <>
2013-02-14 12:20:42 +0000

    xxx

---
+++ bbZ/test/nmssm_2tau2b.C
@@ -0,0 +1,99 @@
+#define nmssm_2tau2b_cxx
+#include "nmssm_2tau2b.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+
+Double_t nmssm_2tau2b::deltaPhi(Double_t phi1, Double_t phi2)
+{
+  Double_t pi = 3.1415927;
+  Double_t dphi = fabs(phi1 - phi2);
+  if(dphi >= pi) dphi = 2. * pi - dphi; 
+  return dphi;
+}
+
+Double_t nmssm_2tau2b::deltaEta(Double_t eta1, Double_t eta2)
+{
+  Double_t deta = fabs(eta1 - eta2);
+  return deta;
+}
+
+Double_t nmssm_2tau2b::deltaR(Double_t eta1, Double_t phi1, Double_t eta2, Double_t phi2)
+{
+  Double_t deta = deltaEta(eta1, eta2);
+  Double_t dphi = deltaPhi(phi1, phi2);
+  Double_t dr = sqrt(deta*deta + dphi*dphi);
+  return dr;
+}
+
+
+void nmssm_2tau2b::Loop()
+{
+//   In a ROOT session, you can do:
+//      Root > .L nmssm_2tau2b.C
+//      Root > nmssm_2tau2b t
+//      Root > t.GetEntry(12); // Fill t data members with entry number 12
+//      Root > t.Show();       // Show values of entry 12
+//      Root > t.Show(16);     // Read and show values of entry 16
+//      Root > t.Loop();       // Loop on all entries
+//
+
+//     This is the loop skeleton where:
+//    jentry is the global entry number in the chain
+//    ientry is the entry number in the current Tree
+//  Note that the argument to GetEntry must be:
+//    jentry for TChain::GetEntry
+//    ientry for TTree::GetEntry and TBranch::GetEntry
+//
+//       To read only selected branches, Insert statements like:
+// METHOD1:
+//    fChain->SetBranchStatus("*",0);  // disable all branches
+//    fChain->SetBranchStatus("branchname",1);  // activate branchname
+// METHOD2: replace line
+//    fChain->GetEntry(jentry);       //read all branches
+//by  b_branchname->GetEntry(ientry); //read only this branch
+
+   TH1F * hPtMu  = new TH1F( "hPtMu", "PtMu", 20, 0., 100.);
+   TH1F * hEtaMu = new TH1F( "hEtaMu", "EtaMu", 50, -5.0, 5.0);
+
+   TH1F * hPtB   = new TH1F( "hPtB", "PtB", 20, 0., 100.);
+   TH1F * hEtaB  = new TH1F( "hEtaB", "EtaB", 50, -5.0, 5.0);
+
+   TH1F * hPtTauH   = new TH1F( "hPtTauH", "PtTauH", 20, 0., 100.);
+   TH1F * hEtaTauH  = new TH1F( "hEtaTauH", "EtaTauH", 50, -5.0, 5.0);
+
+   if (fChain == 0) return;
+
+   Long64_t nentries = fChain->GetEntriesFast();
+
+   Long64_t nbytes = 0, nb = 0;
+   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+      Long64_t ientry = LoadTree(jentry);
+      if (ientry < 0) break;
+      nb = fChain->GetEntry(jentry);   nbytes += nb;
+      // if (Cut(ientry) < 0) continue;
+
+      hPtMu->Fill(ptmu);
+      hEtaMu->Fill(etamu);
+      
+      hPtB->Fill(ptb1);
+      hEtaB->Fill(etab1);
+
+      hPtTauH->Fill(pttauh);
+      hEtaTauH->Fill(etatauh);
+
+   }
+
+   TFile efile("nmssm_60_histos.root","recreate");
+
+   hPtMu->Write();
+   hEtaMu->Write();
+   
+   hPtB->Write();
+   hEtaB->Write();
+   
+   hPtTauH->Write();
+   hEtaTauH->Write();
+   
+   efile.Close();
+}
---
+++ bbZ/test/nmssm_2tau2b.h
@@ -0,0 +1,171 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Thu Feb 14 12:53:01 2013 by ROOT version 5.32/00
+// from TTree t1/analysis tree
+// found on file: nmssm_mh20.root
+//////////////////////////////////////////////////////////
+
+#ifndef nmssm_2tau2b_h
+#define nmssm_2tau2b_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+
+// Header file for the classes stored in the TTree if any.
+
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+class nmssm_2tau2b {
+public :
+
+  Double_t deltaR(Double_t eta1, Double_t phi1, Double_t eta2, Double_t phi2);
+  Double_t deltaEta(Double_t eta1, Double_t eta2);
+  Double_t deltaPhi(Double_t phi1, Double_t phi2);
+
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+   Int_t           fCurrent; //!current Tree number in a TChain
+
+   // Declaration of leaf types
+   Double_t        ptb1;
+   Double_t        etab1;
+   Double_t        phib1;
+   Double_t        ptb2;
+   Double_t        etab2;
+   Double_t        phib2;
+   Double_t        ptmu;
+   Double_t        etamu;
+   Double_t        phimu;
+   Double_t        pttauh;
+   Double_t        etatauh;
+   Double_t        phitauh;
+
+   // List of branches
+   TBranch        *b_ptb1;   //!
+   TBranch        *b_etab1;   //!
+   TBranch        *b_phib1;   //!
+   TBranch        *b_ptb2;   //!
+   TBranch        *b_etab2;   //!
+   TBranch        *b_phib2;   //!
+   TBranch        *b_ptmu;   //!
+   TBranch        *b_etamu;   //!
+   TBranch        *b_phimu;   //!
+   TBranch        *b_pttauh;   //!
+   TBranch        *b_etatauh;   //!
+   TBranch        *b_phitauh;   //!
+
+   nmssm_2tau2b(TTree *tree=0);
+   virtual ~nmssm_2tau2b();
+   virtual Int_t    Cut(Long64_t entry);
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree);
+   virtual void     Loop();
+   virtual Bool_t   Notify();
+   virtual void     Show(Long64_t entry = -1);
+};
+
+#endif
+
+#ifdef nmssm_2tau2b_cxx
+nmssm_2tau2b::nmssm_2tau2b(TTree *tree) : fChain(0) 
+{
+// if parameter tree is not specified (or zero), connect the file
+// used to generate this class and read the Tree.
+   if (tree == 0) {
+     //      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("nmssm_mh20.root");
+     //     TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("nmssm_mh40.root");
+     TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("nmssm_mh60.root");
+      if (!f || !f->IsOpen()) {
+	//         f = new TFile("nmssm_mh20.root");
+	//	f = new TFile("nmssm_mh40.root");
+	f = new TFile("nmssm_mh60.root");
+      }
+      f->GetObject("t1",tree);
+
+   }
+   Init(tree);
+}
+
+nmssm_2tau2b::~nmssm_2tau2b()
+{
+   if (!fChain) return;
+   delete fChain->GetCurrentFile();
+}
+
+Int_t nmssm_2tau2b::GetEntry(Long64_t entry)
+{
+// Read contents of entry.
+   if (!fChain) return 0;
+   return fChain->GetEntry(entry);
+}
+Long64_t nmssm_2tau2b::LoadTree(Long64_t entry)
+{
+// Set the environment to read one entry
+   if (!fChain) return -5;
+   Long64_t centry = fChain->LoadTree(entry);
+   if (centry < 0) return centry;
+   if (fChain->GetTreeNumber() != fCurrent) {
+      fCurrent = fChain->GetTreeNumber();
+      Notify();
+   }
+   return centry;
+}
+
+void nmssm_2tau2b::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fCurrent = -1;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("ptb1", &ptb1, &b_ptb1);
+   fChain->SetBranchAddress("etab1", &etab1, &b_etab1);
+   fChain->SetBranchAddress("phib1", &phib1, &b_phib1);
+   fChain->SetBranchAddress("ptb2", &ptb2, &b_ptb2);
+   fChain->SetBranchAddress("etab2", &etab2, &b_etab2);
+   fChain->SetBranchAddress("phib2", &phib2, &b_phib2);
+   fChain->SetBranchAddress("ptmu", &ptmu, &b_ptmu);
+   fChain->SetBranchAddress("etamu", &etamu, &b_etamu);
+   fChain->SetBranchAddress("phimu", &phimu, &b_phimu);
+   fChain->SetBranchAddress("pttauh", &pttauh, &b_pttauh);
+   fChain->SetBranchAddress("etatauh", &etatauh, &b_etatauh);
+   fChain->SetBranchAddress("phitauh", &phitauh, &b_phitauh);
+   Notify();
+}
+
+Bool_t nmssm_2tau2b::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+void nmssm_2tau2b::Show(Long64_t entry)
+{
+// Print contents of entry.
+// If entry is not specified, print current entry
+   if (!fChain) return;
+   fChain->Show(entry);
+}
+Int_t nmssm_2tau2b::Cut(Long64_t entry)
+{
+// This function may be called from Loop.
+// returns  1 if entry is accepted.
+// returns -1 otherwise.
+   return 1;
+}
+#endif // #ifdef nmssm_2tau2b_cxx
